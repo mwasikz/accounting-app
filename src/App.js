b2f185b5fc6ff1275f2a34d960e908e21ff14355
unknown <imubassir@gmail.com>
2022-04-08 23:29:21 +0600

    test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import './App.css';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App text-3xl font-bold bg-red">
-      Test by Wasik
-=======
-    <div className="App text-3xl font-bold">
-      changed by imubassir
->>>>>>> 2388c9ce689bd3f5de4f3c1dcc8ac58295f1dcf3
-    </div>
+    <div className="App text-3xl font-light shadow-lg pb-3">
+      Accounting App
+  </div>
   );
 }
 
