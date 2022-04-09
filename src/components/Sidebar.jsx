f05256cb3dcccec762752e8198ed4e5a91721538
Mohammad Wasikuzzaman <wasik0003@gmail.com>
2022-04-09 15:05:11 +0600

    t-color

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ import { BsCreditCard2BackFill, BsFillCartFill, BsBank2, BsFileBarGraphFill } fr
 import logo from '../assets/logo.png';
 
 const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
-const isActiveStyle = 'flex items-center px-5 gap-3 font-bold border-r-2 border-red-800 transition-all duration-200 ease-in-out capitalize';
+const isActiveStyle = 'flex items-center px-5 gap-3 font-bold border-r-2 border-red-800 transition-all duration-200 ease-in-out capitalize text-red-800';
 
 
 const Sidebar = ({ closeToggle }) => {
