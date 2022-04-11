42e29cde1cc19e6188c6177ad93567ceb98ca2d6
Mohammad Wasikuzzaman <wasik0003@gmail.com>
2022-04-11 23:36:42 +0600

    db

---
+++ db/accounting_app.sql
@@ -0,0 +1,616 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 11, 2022 at 07:24 PM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `accounting_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `account_type_id` int(11) DEFAULT NULL,
+  `opening_balance` float DEFAULT NULL,
+  `date_created` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account_type`
+--
+
+CREATE TABLE `account_type` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `email` text,
+  `address` text,
+  `city` text,
+  `country` text,
+  `cr_limit_amount` int(11) DEFAULT NULL,
+  `cr_limit_days` int(11) DEFAULT NULL,
+  `remarks` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `estimate`
+--
+
+CREATE TABLE `estimate` (
+  `id` int(11) NOT NULL,
+  `voucher_no` varchar(20) DEFAULT NULL,
+  `date` text,
+  `customer_id` int(11) DEFAULT NULL,
+  `total_amount` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `est_line`
+--
+
+CREATE TABLE `est_line` (
+  `id` int(11) NOT NULL,
+  `estimate_id` int(11) DEFAULT NULL,
+  `line_num` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `rate` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expense`
+--
+
+CREATE TABLE `expense` (
+  `id` int(11) NOT NULL,
+  `voucher_no` varchar(20) DEFAULT NULL,
+  `date` text,
+  `account_id` int(11) DEFAULT NULL,
+  `total_amount` float DEFAULT NULL,
+  `remarks` text,
+  `ref_doc_name` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exp_line`
+--
+
+CREATE TABLE `exp_line` (
+  `id` int(11) NOT NULL,
+  `expense_id` int(11) DEFAULT NULL,
+  `line_num` int(11) DEFAULT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `amount` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `income`
+--
+
+CREATE TABLE `income` (
+  `id` int(11) NOT NULL,
+  `voucher_no` varchar(20) DEFAULT NULL,
+  `date` text,
+  `account_id` int(11) DEFAULT NULL,
+  `total_amount` float DEFAULT NULL,
+  `remarks` text,
+  `ref_doc_name` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inc_line`
+--
+
+CREATE TABLE `inc_line` (
+  `id` int(11) NOT NULL,
+  `income_id` int(11) DEFAULT NULL,
+  `line_num` int(11) DEFAULT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `amount` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `rate` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `po_line`
+--
+
+CREATE TABLE `po_line` (
+  `id` int(11) NOT NULL,
+  `pur_order_id` int(11) DEFAULT NULL,
+  `line_num` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `rate` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `code` text,
+  `name` varchar(50) DEFAULT NULL,
+  `desc` text,
+  `unit_id` int(11) DEFAULT NULL,
+  `default_pur_price` float DEFAULT NULL,
+  `default_sale_price` float DEFAULT NULL,
+  `stock_open_qty` int(11) DEFAULT NULL,
+  `stock_open_val` float DEFAULT NULL,
+  `stock_open_date` text,
+  `serial_track` tinyint(1) DEFAULT NULL,
+  `reorder_qty` int(11) DEFAULT NULL,
+  `remarks` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase`
+--
+
+CREATE TABLE `purchase` (
+  `id` int(11) NOT NULL,
+  `voucher_no` varchar(20) DEFAULT NULL,
+  `date` text,
+  `supplier_id` int(11) DEFAULT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `total_amount` float DEFAULT NULL,
+  `total_discount` float DEFAULT NULL,
+  `round_off` float DEFAULT NULL,
+  `remarks` text,
+  `ref_doc_name` text,
+  `terms_conditions` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pur_line`
+--
+
+CREATE TABLE `pur_line` (
+  `id` int(11) NOT NULL,
+  `purchase_id` int(11) DEFAULT NULL,
+  `line_num` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `rate` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pur_order`
+--
+
+CREATE TABLE `pur_order` (
+  `id` int(11) NOT NULL,
+  `voucher_no` varchar(20) DEFAULT NULL,
+  `date` text,
+  `supplier_id` int(11) DEFAULT NULL,
+  `total_amount` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sale`
+--
+
+CREATE TABLE `sale` (
+  `id` int(11) NOT NULL,
+  `voucher_no` varchar(20) DEFAULT NULL,
+  `date` text,
+  `customer_id` int(11) DEFAULT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `total_amount` float DEFAULT NULL,
+  `total_discount` float DEFAULT NULL,
+  `round_off` float DEFAULT NULL,
+  `remarks` text,
+  `ref_doc_name` text,
+  `terms_conditions` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sale_line`
+--
+
+CREATE TABLE `sale_line` (
+  `id` int(11) NOT NULL,
+  `sale_id` int(11) DEFAULT NULL,
+  `line_num` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `rate` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `serial`
+--
+
+CREATE TABLE `serial` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `pur_line_id` int(11) DEFAULT NULL,
+  `serial_no` varchar(50) DEFAULT NULL,
+  `sale_line_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `id` int(11) NOT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `email` text,
+  `address` text,
+  `city` text,
+  `country` text,
+  `cr_limit_amount` int(11) DEFAULT NULL,
+  `cr_limit_days` int(11) DEFAULT NULL,
+  `remarks` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unit`
+--
+
+CREATE TABLE `unit` (
+  `id` int(11) NOT NULL,
+  `name` varchar(10) DEFAULT NULL,
+  `symbol` varchar(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `account_type_id` (`account_type_id`);
+
+--
+-- Indexes for table `account_type`
+--
+ALTER TABLE `account_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `account_id` (`account_id`);
+
+--
+-- Indexes for table `estimate`
+--
+ALTER TABLE `estimate`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customer_id` (`customer_id`);
+
+--
+-- Indexes for table `est_line`
+--
+ALTER TABLE `est_line`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `estimate_id` (`estimate_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `expense`
+--
+ALTER TABLE `expense`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `account_id` (`account_id`);
+
+--
+-- Indexes for table `exp_line`
+--
+ALTER TABLE `exp_line`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `expense_id` (`expense_id`),
+  ADD KEY `account_id` (`account_id`);
+
+--
+-- Indexes for table `income`
+--
+ALTER TABLE `income`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `account_id` (`account_id`);
+
+--
+-- Indexes for table `inc_line`
+--
+ALTER TABLE `inc_line`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `income_id` (`income_id`),
+  ADD KEY `account_id` (`account_id`);
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `po_line`
+--
+ALTER TABLE `po_line`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pur_order_id` (`pur_order_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `unit_id` (`unit_id`);
+
+--
+-- Indexes for table `purchase`
+--
+ALTER TABLE `purchase`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `account_id` (`account_id`),
+  ADD KEY `supplier_id` (`supplier_id`);
+
+--
+-- Indexes for table `pur_line`
+--
+ALTER TABLE `pur_line`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `purchase_id` (`purchase_id`);
+
+--
+-- Indexes for table `pur_order`
+--
+ALTER TABLE `pur_order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplier_id` (`supplier_id`);
+
+--
+-- Indexes for table `sale`
+--
+ALTER TABLE `sale`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `account_id` (`account_id`);
+
+--
+-- Indexes for table `sale_line`
+--
+ALTER TABLE `sale_line`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `sale_id` (`sale_id`);
+
+--
+-- Indexes for table `serial`
+--
+ALTER TABLE `serial`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `sale_line_id` (`sale_line_id`),
+  ADD KEY `pur_line_id` (`pur_line_id`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `account_id` (`account_id`);
+
+--
+-- Indexes for table `unit`
+--
+ALTER TABLE `unit`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `account`
+--
+ALTER TABLE `account`
+  ADD CONSTRAINT `account_ibfk_1` FOREIGN KEY (`account_type_id`) REFERENCES `account_type` (`id`);
+
+--
+-- Constraints for table `customer`
+--
+ALTER TABLE `customer`
+  ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`);
+
+--
+-- Constraints for table `estimate`
+--
+ALTER TABLE `estimate`
+  ADD CONSTRAINT `estimate_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+
+--
+-- Constraints for table `est_line`
+--
+ALTER TABLE `est_line`
+  ADD CONSTRAINT `est_line_ibfk_1` FOREIGN KEY (`estimate_id`) REFERENCES `estimate` (`id`),
+  ADD CONSTRAINT `est_line_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+--
+-- Constraints for table `expense`
+--
+ALTER TABLE `expense`
+  ADD CONSTRAINT `expense_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`);
+
+--
+-- Constraints for table `exp_line`
+--
+ALTER TABLE `exp_line`
+  ADD CONSTRAINT `exp_line_ibfk_1` FOREIGN KEY (`expense_id`) REFERENCES `expense` (`id`),
+  ADD CONSTRAINT `exp_line_ibfk_2` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`);
+
+--
+-- Constraints for table `income`
+--
+ALTER TABLE `income`
+  ADD CONSTRAINT `income_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`);
+
+--
+-- Constraints for table `inc_line`
+--
+ALTER TABLE `inc_line`
+  ADD CONSTRAINT `inc_line_ibfk_1` FOREIGN KEY (`income_id`) REFERENCES `income` (`id`),
+  ADD CONSTRAINT `inc_line_ibfk_2` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`);
+
+--
+-- Constraints for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+--
+-- Constraints for table `po_line`
+--
+ALTER TABLE `po_line`
+  ADD CONSTRAINT `po_line_ibfk_1` FOREIGN KEY (`pur_order_id`) REFERENCES `pur_order` (`id`),
+  ADD CONSTRAINT `po_line_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`unit_id`) REFERENCES `unit` (`id`);
+
+--
+-- Constraints for table `purchase`
+--
+ALTER TABLE `purchase`
+  ADD CONSTRAINT `purchase_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`),
+  ADD CONSTRAINT `purchase_ibfk_2` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`id`);
+
+--
+-- Constraints for table `pur_line`
+--
+ALTER TABLE `pur_line`
+  ADD CONSTRAINT `pur_line_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `pur_line_ibfk_2` FOREIGN KEY (`purchase_id`) REFERENCES `purchase` (`id`);
+
+--
+-- Constraints for table `pur_order`
+--
+ALTER TABLE `pur_order`
+  ADD CONSTRAINT `pur_order_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`id`);
+
+--
+-- Constraints for table `sale`
+--
+ALTER TABLE `sale`
+  ADD CONSTRAINT `sale_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
+  ADD CONSTRAINT `sale_ibfk_2` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`);
+
+--
+-- Constraints for table `sale_line`
+--
+ALTER TABLE `sale_line`
+  ADD CONSTRAINT `sale_line_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `sale_line_ibfk_2` FOREIGN KEY (`sale_id`) REFERENCES `sale` (`id`);
+
+--
+-- Constraints for table `serial`
+--
+ALTER TABLE `serial`
+  ADD CONSTRAINT `serial_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `serial_ibfk_2` FOREIGN KEY (`sale_line_id`) REFERENCES `sale_line` (`id`),
+  ADD CONSTRAINT `serial_ibfk_3` FOREIGN KEY (`pur_line_id`) REFERENCES `pur_line` (`id`);
+
+--
+-- Constraints for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD CONSTRAINT `supplier_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
